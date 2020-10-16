7c7e453579adf8164d18346f3624eeb2659ed642
FahmidaNoor <fahmidanoor7fnm@gmail.com>
2020-10-17 00:39:38 +0600

    Login Module

---
+++ src/main/java/com/bjit/SeleniumOnlineProject2/Utils/DriverManager.java
@@ -0,0 +1,18 @@
+package com.bjit.SeleniumOnlineProject2.Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+	
+	static {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Fahmida\\Desktop\\Selenium Files\\chromedriver_win32\\chromedriver.exe");
+	}
+	
+	public static WebDriver driver= new ChromeDriver();
+	
+	private DriverManager()
+	{
+		
+	}
+}
